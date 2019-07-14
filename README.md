36ab0e5d204d1180d57d1b7d5640e44db69cec15
C.Bennett <carole.bennett@enosix.com>
2019-07-14 18:50:40 -0500

    merge readme file

--- README.md
+++ README.md
@@ -66,3 +66,5 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+# iframe-test
+Just a quick test to evaluate a react component in iframe
--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button name="test1">onComponentInit</button>&nbsp;
+          <button name="test2">onBeforeCall</button>&nbsp;
+          <button name="test3">onDataProcessed</button>&nbsp;
+          <button name="test4">onAfterCall</button>&nbsp;
+          <button name="test5">onComponentDestruct</button> &nbsp; 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
