6164a9b421f79d1900f4159ac5cd86293dd8541c
C.Bennett <carole.bennett@enosix.com>
2019-07-24 16:17:34 -0500

    Adding functions to send window messages

--- src/App.js
+++ src/App.js
@@ -2,14 +2,40 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  function onComponentInit()
+  {
+    window.postMessage('A');
+  }
+
+  function onBeforeCall()
+  {
+    window.postMessage('B');
+  }
+
+  function onDataProcessed()
+  {
+    window.postMessage('C');
+  }
+
+  function onAfterCall()
+  {
+    window.postMessage('D');
+  }
+
+  function onComponentDestruct()
+  {
+    window.postMessage('E');
+  }
+
   return (
     <div className="App">
         <p>
-          <button name="test1">onComponentInit</button>&nbsp;
-          <button name="test2">onBeforeCall</button>&nbsp;
-          <button name="test3">onDataProcessed</button>&nbsp;
-          <button name="test4">onAfterCall</button>&nbsp;
-          <button name="test5">onComponentDestruct</button> &nbsp; 
+          <button name="test1" onClick={onComponentInit()}>onComponentInit</button>&nbsp;
+          <button name="test2" onClick={onBeforeCall()}>onBeforeCall</button>&nbsp;
+          <button name="test3"onClick={onDataProcessed()}>onDataProcessed</button>&nbsp;
+          <button name="test4" onClick={onAfterCall()}>onAfterCall</button>&nbsp;
+          <button name="test5"onClick={onComponentDestruct()}>onComponentDestruct</button> &nbsp; 
         </p>
     </div>
   );
